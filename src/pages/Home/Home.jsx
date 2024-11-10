7b705090fd24d406608b6ebb8bc2b968fd8db158
Dilmurd <0nishonov.dilmurod@gmail.com>
2024-11-11 00:31:48 +0500

    ozgartirdim

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sale from '../../components/Sale/Sale'
 import Catalog from '../../components/Catalog/Catalog'
+import Menu from "../../components/Menu/Menu"
 
 function Home() {
   return (
     <>
     <Sale/> 
     <Catalog/>
+    <Menu/>
     </>
   )
 }
